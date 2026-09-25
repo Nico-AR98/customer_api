8ea91c94a8526b91695f24edcfe863ec9d29ec39
Nicolás Gallardo <ngallardo@leafnoise.io>
2026-09-25 13:30:58 -0300

    database config

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default='sqlite://db.sqlite3',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
